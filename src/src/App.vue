b7b4e84d1d6d6b96a8c1e623c079bb2df63c04f7
verybigboy <55190546+verybigboy@users.noreply.github.com>
2025-06-03 09:53:59 +0800

    update src file from pakeplus

---
+++ src/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <iframe
+    ref="myIframe"
+    src="https://dify.xsayai.com/chat/SqPwUWEKvMFSacNe"
+    frameborder="0"
+    scrolling="no"
+  ></iframe>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
