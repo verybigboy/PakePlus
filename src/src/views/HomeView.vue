1bec650cd24c61648672c7c3e4d9ededc8871d5d
verybigboy <55190546+verybigboy@users.noreply.github.com>
2025-06-03 09:54:26 +0800

    update src file from pakeplus

---
+++ src/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <iframe
+    ref="myIframe"
+    src="https://dify.xsayai.com/chat/SqPwUWEKvMFSacNe"
+    width="100%"
+    height="100%"
+    frameborder="0"
+    scrolling="no"
+  ></iframe>
+</template>
